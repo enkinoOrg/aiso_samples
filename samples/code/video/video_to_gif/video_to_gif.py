0f3e4b8a65de9f3b4fafd21e8872ee5b4cfb9d9b
namth <wase894@gmail.com>
2022-09-05 10:01:41 +0900

    test: gif 샘플코드 업데이트

--- samples/code/video/video_to_gif/video_to_gif.py
+++ samples/code/video/video_to_gif/video_to_gif.py
@@ -10,10 +10,12 @@ gif의 프레임은 15으로 고정되어 있습니다.
 
 import cv2
 import numpy as np
+import math
 
-def fun(input_data, running_time=30):
+def fun(input_data, start_sec=0, end_sec=0):
     # 입력으로 video의 path가 전달됩니다.
     # running_time gif파일의 재싱시간을 설정합니다.
+
     cap = cv2.VideoCapture(input_data[0])
 
     video_fps = cap.get(cv2.CAP_PROP_FPS)
@@ -21,10 +23,21 @@ def fun(input_data, running_time=30):
     video_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     video_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
+    if(end_sec == 0):
+        end_sec = math.ceil(video_frame / video_fps)
+    
+    # 웹 페이지에 설정한 에러메시지가 출력됩니다.
+    if(start_sec > end_sec):
+        raise Exception("end_sec is smaller than start_sec")
+
     # 프레임 고정
     gif_frame_rate = 15
-    gif_need_frame = gif_frame_rate * running_time
     fps_ratio = round(video_fps / gif_frame_rate)
+
+    start_frame = round(video_fps * start_sec)
+    end_frame = round(video_fps * end_sec) - start_frame
+
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
 
     ret = True
     total_frame_cnt = 0
@@ -45,8 +58,8 @@ def fun(input_data, running_time=30):
             frame = cv2.resize(frame, (dst_width, dst_height), cv2.INTER_AREA)
             frames.append(frame)
             captured += 1
-
-        if(captured > gif_need_frame):
+        
+        if(total_frame_cnt > end_frame):
             break
 
         total_frame_cnt += 1
