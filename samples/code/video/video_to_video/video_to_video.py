f4fb3a1e7e9d37ca197b06a3e8a7f640feaa15f3
namth <wase894@gmail.com>
2022-09-06 16:22:08 +0900

    test: 비디오 샘플코드 추가

---
+++ samples/code/video/video_to_video/video_to_video.py
@@ -0,0 +1,27 @@
+## video 테스트 중 입니다.
+## 이후 아래의 코드가 제대로 작동이 되지 않을 수도 있습니다.
+
+import cv2
+import numpy as np
+
+def fun(input_data):
+    input_video_path = input_data[0]
+    cap = cv2.VideoCapture(input_video_path)
+
+    dst_width = 640
+    dst_height = 480
+
+    frames = []
+    ret = True
+
+    while ret:
+        ret, frame = cap.read()
+        if(np.shape(frame) == ()):
+            continue
+        resized_frame = cv2.resize(frame, (dst_width, dst_height), cv2.INTER_AREA)
+        frames.append(resized_frame)
+
+    if(cap.isOpened()):
+        cap.release()
+
+    return [frames]
